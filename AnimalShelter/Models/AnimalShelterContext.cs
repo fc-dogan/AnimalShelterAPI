7ad9f71ab05f8e4a3896da9572e910a41c43a44f
Fatma <fceyda@gmail.com>
2020-04-03 08:41:31 -0700

    create Models/animal.cs

---
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Models
+{
+    public class AnimalShelterContext : DbContext
+    {
+        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
