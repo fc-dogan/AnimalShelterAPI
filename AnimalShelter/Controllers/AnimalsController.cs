2ccae9c0d9c04b1fcb4f6856f986f1793c0b0215
Fatma <fceyda@gmail.com>
2020-04-03 09:38:12 -0700

    create animalscontroller

---
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/animals
+    [HttpGet]
+    public ActionResult<IEnumerable<Animal>> Get()
+    {
+      return _db.Animals.ToList();
+    }
+
+    // POST api/animals
+    [HttpPost]
+    public void Post([FromBody] Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+    }
+  }
+}
