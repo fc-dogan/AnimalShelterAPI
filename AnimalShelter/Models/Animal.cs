3a3ebe56904d8e43fde33cdefdcd808f3e09fe1c
Fatma <fceyda@gmail.com>
2020-04-03 09:32:01 -0700

    add migrations and create database

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -4,7 +4,7 @@ namespace AnimalShelter.Models
     {
         public int AnimalId { get; set; }
         public string Name { get; set; }
-        public string Species { get; set; }
+        public string Type { get; set; }
         public int Age { get; set; }
         public string Gender { get; set; }
     }
