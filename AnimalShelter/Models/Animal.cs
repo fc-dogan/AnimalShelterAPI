c36866e511112ea259c0216db43bcdea2bb457ff
Fatma <fceyda@gmail.com>
2020-04-03 10:15:27 -0700

    add validations to animal class

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelter.Models
 {
     public class Animal
     {
         public int AnimalId { get; set; }
+
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
+        [Required]
         public string Type { get; set; }
+        [Required]
         public int Age { get; set; }
+        [Required]
         public string Gender { get; set; }
     }
 }
