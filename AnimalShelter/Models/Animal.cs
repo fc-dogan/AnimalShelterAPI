5ac86de305ae66b686b0f2cdd994d2aedaf2fd32
Fatma <fceyda@gmail.com>
2020-04-03 16:44:33 -0700

    add age range validation and error message

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -12,6 +12,7 @@ namespace AnimalShelter.Models
         [Required]
         public string Type { get; set; }
         [Required]
+        [Range(0, 100, ErrorMessage = "Age must be between 1 and 100.")]
         public int Age { get; set; }
         [Required]
         public string Gender { get; set; }
