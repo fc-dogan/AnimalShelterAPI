e799a7dc3c550a134fd6358414f1e08dad0994e7
Fatma <fceyda@gmail.com>
2020-04-03 09:42:08 -0700

    add HttpPut route to animalscontroller

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using AnimalShelter.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelter.Controllers
 {
@@ -30,5 +31,22 @@ namespace AnimalShelter.Controllers
       _db.Animals.Add(animal);
       _db.SaveChanges();
     }
+
+    // GET api/animals/5
+    [HttpGet("{id}")]
+    public ActionResult<Animal> Get(int id)
+    {
+        return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+    }
+
+     // PUT api/animals/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Animal animal)
+    {
+        animal.AnimalId = id;
+        _db.Entry(animal).State = EntityState.Modified;
+        _db.SaveChanges();
+    }
+
   }
 }
