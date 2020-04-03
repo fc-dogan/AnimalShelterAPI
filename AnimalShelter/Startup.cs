2bdd1fed6d8c15338cc325f6a3da37c317ee3a02
Fatma <fceyda@gmail.com>
2020-04-03 13:26:43 -0700

    add API Versioning sevice to  configureService

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -30,6 +30,11 @@ namespace AnimalShelter
             services.AddDbContext<AnimalShelterContext>(opt =>
                 opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddApiVersioning(o => {
+                o.ReportApiVersions = true;
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.DefaultApiVersion = new ApiVersion(1, 0);
+                });
 
             // Register the Swagger services
             services.AddSwaggerDocument(config =>
